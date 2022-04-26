1c5e0555a80aa15dd0e765a8084d0af83f0a488a
brinkqiang <brink.qiang@gmail.com>
2022-04-26 19:51:51 +0800

    add

--- bin/script/test.lua
+++ bin/script/test.lua
@@ -2,8 +2,7 @@
 local luahttpclient = require("luahttpclient")
 local msg = {wd = "http"}
 
-local ret = luahttpclient.post("http://www.baidu.com", msg)
+local ret = luahttpclient.post("http://www.baidu.com")
 print(ret)
-
 local ret2 = luahttpclient.get("http://baidu.com")
 print(ret2)
